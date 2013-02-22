fc79a277e9bd66044f79e3d2ba76862a5a88d630
jhgilbert <j.h.gilbert@gmail.com>
2013-02-21 23:33:21 -0800

    Add crude task time-parsing method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+  def parse_total_time(time)
+    # skipping the modulus for speed
+    if time < 60
+      "#{time} seconds"
+    elsif time < 3600
+      "#{(time/60.0).round} minutes"
+    else
+      "#{(time/3600.0).round} hours"
+    end
+  end
 end
