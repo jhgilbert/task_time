e54014d061a3408f29fe43a273878c5e031f467f
jhgilbert <j.h.gilbert@gmail.com>
2013-02-21 18:59:23 -0800

    Rough in initial start/stop definitions

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,20 @@
 class PagesController < ApplicationController
   def home
-    @tasks = Task.all
+    @tasks = Task.all    
   end
+  
+  def start
+    task = Task.find(params[:id])
+    task.start_time = DateTime.now
+    task.running = true
+  end
+  
+  def stop
+    task = Task.find(params[:id])
+    task.running = false
+    task.stop_time = DateTime.now
+    time_elapsed = task.stop_time - task.start_time
+    task.total_time = task.total_time + time_elapsed  
+  end
+  
 end
