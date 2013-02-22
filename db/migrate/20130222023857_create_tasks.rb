d946bbe4db9a1551eb098225dd1285ddad132a0a
jhgilbert <j.h.gilbert@gmail.com>
2013-02-21 18:52:25 -0800

    Add Task attribute defaults

--- db/migrate/20130222023857_create_tasks.rb
+++ db/migrate/20130222023857_create_tasks.rb
@@ -3,8 +3,8 @@ class CreateTasks < ActiveRecord::Migration
     create_table :tasks do |t|
       t.datetime :start_time
       t.datetime :stop_time
-      t.boolean :running
-      t.integer :total_time
+      t.boolean :running, :default => false
+      t.integer :total_time, :default => 0
 
       t.timestamps
     end
